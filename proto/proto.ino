b97128449ee6c14b337c9c88abca27cb64693788
AtsushiNi <45954308+AtsushiNi@users.noreply.github.com>
2019-11-13 10:01:58 +0900

    'PID制御'

--- proto/proto.ino
+++ proto/proto.ino
@@ -18,6 +18,13 @@ float reduceRacio = 0.2; // ラジコンモードでの左右減速比
 int outputMoterPower = 0; // モーター出力値を出力するかどうか 0:しない, 1:する
 int outputLightSensor = 1; // 光センサーの値を出力するかどうか 0:しない, 1:する
 int outputMode = 0; // 表示出力の様式　0:ArduinoIDE, 1:Android, 2:ターミナル
+float kp = 0; // 比例制御のパラメータ
+float ki = 0; // 積分制御のパラメータ
+float kd = 0; // 微分制御のパラメータ
+
+// PID制御の変数
+float pos_1 = 0; // 前回の位置
+float pos_2 = 0; // 前々回の位置
 
 void setup() {
   Serial.begin(9600);
@@ -37,28 +44,37 @@ void loop() {
         switch (inputMode) {
           case 'b':
             basicStatus = atoi(tmp);
-          break;
+            break;
           case 'c':
             runMode = atoi(tmp);
-          break;
+            break;
           case 'd':
             radioControllDirection = atoi(tmp);
-          break;
+            break;
           case 'e':
             basicSpeed = atoi(tmp);
-          break;
+            break;
           case 'f':
             reduceRacio = atof(tmp);
-          break;
+            break;
           case 'g':
             outputMoterPower = atoi(tmp);
-          break;
+            break;
           case 'h':
             outputMode = atoi(tmp);
-          break;
+            break;
           case 'i':
             outputLightSensor = atoi(tmp);
-          break;
+            break;
+          case 'j':
+            kp = atof(tmp);
+            break;
+          case 'k':
+            ki = atof(tmp);
+            break;
+          case 'l':
+            kd = atof(tmp);
+            break;
         }
         inputPointer = 0;
       }
@@ -75,14 +91,25 @@ void loop() {
   int leftData = analogRead(leftSensorPin);
 
   // 現在位置の算出
-  float position = 0; //線上が0. 機体の右へのずれが正. 両端のセンサー位置が±1
-  position = calcuPosBy2(rightData, leftData);
+  float pos = 0; //線上が0. 機体の右へのずれが正. 両端のセンサー位置が±1
+  pos = calcuPosBy2(rightData, leftData);
 
   // モーター出力計算
   float rightPower = 0; // 右モーター出力
   float leftPower = 0; // 左モーター出力
 
-  if (runMode == 1) {
+  if (runMode == 0) {
+    // PID制御
+    float du; // 制御量の変化
+    float u; // 制御量
+    du = kp * (pos - pos_1) + pi * pos + pd * (pos - 2 * pos_1 + pis_2);
+    pos_2 = pos_1;
+    pos_1 = pos;
+    u = u + du;
+    // 制御量からモーター出力量
+    rightPower = basicSpeed + u;
+    leftPower = basicSpeed - u;
+  } else if (runMode == 1) {
     rightPower = (float)basicSpeed;
     leftPower = (float)basicSpeed;
     switch (radioControllDirection) {
