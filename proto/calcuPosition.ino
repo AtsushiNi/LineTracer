3721f799ea457d225625cde7885f31110a4a7028
AtsushiNi <45954308+AtsushiNi@users.noreply.github.com>
2019-12-06 13:22:23 +0900

    整理

--- proto/calcuPosition.ino
+++ proto/calcuPosition.ino
@@ -28,68 +28,6 @@ void setBorders(int *borders) {
   }
 }
 
-// 二つのセンサーから現在位置を割り出す
-// float calcuPosBy2(int rightData, int leftData, float *res) {
-//   float position = 0;
-//   float likelyPos[2][2];
-//   float distances[4];
-  
-//   // それぞれのセンサーによる予想位置の算出(likelyPosを書き換える)
-//   calcuLikelyPos(1, rightData, likelyPos[0]);
-//   calcuLikelyPos(-1, leftData, likelyPos[1]);
-//   res[0] = likelyPos[0][0];
-//   res[1] = likelyPos[0][1];
-//   res[2] = likelyPos[1][0];
-//   res[3] = likelyPos[1][1];
-//   // 予想位置同士の差を計算
-//   distances[0] = fabsf(likelyPos[0][0] - likelyPos[1][0]);
-//   distances[1] = fabsf(likelyPos[0][0] - likelyPos[1][1]);
-//   distances[2] = fabsf(likelyPos[0][1] - likelyPos[1][0]);
-//   distances[3] = fabsf(likelyPos[0][1] - likelyPos[1][1]);
-//   // 差の一番小さいものを特定
-//   int length = sizeof(distances) / sizeof(float);
-//   float min = distances[0];
-//   int minIndex = 0;
-//   for(int i=1; i<length; i++) {
-//     if (distances[i] < min) {
-//       min = distances[i];
-//       minIndex = i;
-//     }
-//   }
-//   switch (minIndex) {
-//     case 0:
-//       position = (likelyPos[0][0] + likelyPos[1][0])/2.0;
-//       break;
-//     case 1:
-//       position = (likelyPos[0][0] + likelyPos[1][1])/2.0;
-//       break;
-//     case 2:
-//       position = (likelyPos[0][1] + likelyPos[1][0])/2.0;
-//       break;
-//     case 3:
-//       position = (likelyPos[0][1] + likelyPos[1][1])/2.0;
-//       break;
-//     default:
-//       position = 0;
-//   }
-
-//   return position;
-// }
-
-// センサーの位置とそのセンサーのデータから機体の予想位置を二つ算出する sensorPos: -1~+1. 右が正
-// void calcuLikelyPos(float sensorPos, int data, float likelyPos[2]) {
-//   likelyPos[0] = 1 - data/1000.0;
-//   likelyPos[1] = -likelyPos[0];
-
-//   likelyPos[0] += sensorPos;
-//   likelyPos[1] += sensorPos;
-
-//   if (data < 50) {
-//     likelyPos[0] = 100;
-//     likelyPos[1] = -100;
-//   }
-// }
-
 // 4つのセンサーから現在位置を割り出す。
 // sensorDatas: 各センサーの値。右のセンサーから順。
 // polyfit_params: python側で計算した、センサ値-現在位置の近似式の係数。[センサー][{0次,1次}]
@@ -175,6 +113,9 @@ float calcuPosBy4(int *sensorDatas) {
     }
     return position;
     break;
+  case 4:
+    return 404;
+    break;
   default:
     return 0;
     break;
@@ -218,5 +159,67 @@ float calcuLikelyPosFor4(float sensorPos, int data, float params_0, float params
 //   } else {
 //     // light4が一番小さい
 //     return 1
+//   }
+// }
+
+// 二つのセンサーから現在位置を割り出す
+// float calcuPosBy2(int rightData, int leftData, float *res) {
+//   float position = 0;
+//   float likelyPos[2][2];
+//   float distances[4];
+  
+//   // それぞれのセンサーによる予想位置の算出(likelyPosを書き換える)
+//   calcuLikelyPos(1, rightData, likelyPos[0]);
+//   calcuLikelyPos(-1, leftData, likelyPos[1]);
+//   res[0] = likelyPos[0][0];
+//   res[1] = likelyPos[0][1];
+//   res[2] = likelyPos[1][0];
+//   res[3] = likelyPos[1][1];
+//   // 予想位置同士の差を計算
+//   distances[0] = fabsf(likelyPos[0][0] - likelyPos[1][0]);
+//   distances[1] = fabsf(likelyPos[0][0] - likelyPos[1][1]);
+//   distances[2] = fabsf(likelyPos[0][1] - likelyPos[1][0]);
+//   distances[3] = fabsf(likelyPos[0][1] - likelyPos[1][1]);
+//   // 差の一番小さいものを特定
+//   int length = sizeof(distances) / sizeof(float);
+//   float min = distances[0];
+//   int minIndex = 0;
+//   for(int i=1; i<length; i++) {
+//     if (distances[i] < min) {
+//       min = distances[i];
+//       minIndex = i;
+//     }
+//   }
+//   switch (minIndex) {
+//     case 0:
+//       position = (likelyPos[0][0] + likelyPos[1][0])/2.0;
+//       break;
+//     case 1:
+//       position = (likelyPos[0][0] + likelyPos[1][1])/2.0;
+//       break;
+//     case 2:
+//       position = (likelyPos[0][1] + likelyPos[1][0])/2.0;
+//       break;
+//     case 3:
+//       position = (likelyPos[0][1] + likelyPos[1][1])/2.0;
+//       break;
+//     default:
+//       position = 0;
+//   }
+
+//   return position;
+// }
+
+// センサーの位置とそのセンサーのデータから機体の予想位置を二つ算出する sensorPos: -1~+1. 右が正
+// void calcuLikelyPos(float sensorPos, int data, float likelyPos[2]) {
+//   likelyPos[0] = 1 - data/1000.0;
+//   likelyPos[1] = -likelyPos[0];
+
+//   likelyPos[0] += sensorPos;
+//   likelyPos[1] += sensorPos;
+
+//   if (data < 50) {
+//     likelyPos[0] = 100;
+//     likelyPos[1] = -100;
 //   }
 // }
